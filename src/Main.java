3130cd9bfaba0d9dcb35f9fee6ad42797d864f4b
alisad <asgh1381@gmail.com>
2024-06-29 11:53:27 +0330

    read file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        File dir = new File("src/resources");
+        File[] directoryListing = dir.listFiles();
+        if (directoryListing != null) {
+            for (File child : directoryListing) {
+                BufferedReader br = new BufferedReader(new FileReader(child));
+                String line;
+                while ((line = br.readLine()) != null) {
+
+                }
+
+            }
+        } else {
+            throw new RuntimeException("path isn't a directory");
+        }
+
+
+        File file = new File("src/resources/Clean Agile.txt");
+        System.out.println(file.exists());
+
     }
 }
