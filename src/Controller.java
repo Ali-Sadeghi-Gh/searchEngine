48805f5449fe53d51dfaa05d2825c7817da49647
alisad <asgh1381@gmail.com>
2024-06-29 13:39:11 +0330

    add createTable function

--- src/Controller.java
+++ src/Controller.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Vector;
 
@@ -10,5 +12,31 @@ public class Controller {
             instance = new Controller();
         }
         return instance;
+    }
+
+    public void createTable() throws IOException {
+        table = new HashMap<>();
+
+        File dir = new File("src/resources");
+        File[] directoryListing = dir.listFiles();
+        if (directoryListing != null) {
+            for (File child : directoryListing) {
+                String[] words = FileToString.readFile(child);
+                String fileName = child.getName();
+                for (String word : words) {
+                    if (table.containsKey(word)) {
+                        if (!table.get(word).lastElement().equals(fileName)) {
+                            table.get(word).add(fileName);
+                        }
+                    } else {
+                        Vector<String> vector = new Vector<>();
+                        vector.add(fileName);
+                        table.put(word, vector);
+                    }
+                }
+            }
+        } else {
+            throw new RuntimeException("path isn't a directory");
+        }
     }
 }
