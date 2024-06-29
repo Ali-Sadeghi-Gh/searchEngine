3794da09d3db7ed808f7ae2bada3e8e9a6f3b14b
alisad <asgh1381@gmail.com>
2024-06-29 13:43:10 +0330

    add createTable and handleQuery to Main

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,17 @@
 import java.io.*;
-import java.util.HashMap;
-import java.util.Vector;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        File dir = new File("src/resources");
-        File[] directoryListing = dir.listFiles();
-        HashMap<String, Vector<String>> table = new HashMap<>();
-        if (directoryListing != null) {
-            for (File child : directoryListing) {
-                String[] words = FileToString.readFile(child);
-                String fileName = child.getName();
-                for (String word : words) {
-                    if (table.containsKey(word)) {
-                        if (!table.get(word).lastElement().equals(fileName)) {
-                            table.get(word).add(fileName);
-                        }
-                    } else {
-                        Vector<String> vector = new Vector<>();
-                        vector.add(fileName);
-                        table.put(word, vector);
-                    }
-                }
+        Controller.getInstance().createTable();
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String query = scanner.next();
+            if (query.equals("q")) {
+                break;
             }
-        } else {
-            throw new RuntimeException("path isn't a directory");
+            Controller.getInstance().handleQuery(query);
         }
-
-
-        System.out.println(table.get("engineering"));
-
     }
 }
