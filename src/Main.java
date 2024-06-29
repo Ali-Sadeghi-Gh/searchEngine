53732e863fc3eab5f68eca163540880fc64ab2e9
alisad <asgh1381@gmail.com>
2024-06-29 13:33:03 +0330

    implement createTable functionality

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,34 @@
 import java.io.*;
+import java.util.HashMap;
+import java.util.Vector;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         File dir = new File("src/resources");
         File[] directoryListing = dir.listFiles();
+        HashMap<String, Vector<String>> table = new HashMap<>();
         if (directoryListing != null) {
             for (File child : directoryListing) {
-                BufferedReader br = new BufferedReader(new FileReader(child));
-                String line;
-                while ((line = br.readLine()) != null) {
-
+                String[] words = FileToString.readFile(child);
+                String fileName = child.getName();
+                for (String word : words) {
+                    if (table.containsKey(word)) {
+                        if (!table.get(word).lastElement().equals(fileName)) {
+                            table.get(word).add(fileName);
+                        }
+                    } else {
+                        Vector<String> vector = new Vector<>();
+                        vector.add(fileName);
+                        table.put(word, vector);
+                    }
                 }
-
             }
         } else {
             throw new RuntimeException("path isn't a directory");
         }
 
 
-        File file = new File("src/resources/Clean Agile.txt");
-        System.out.println(file.exists());
+        System.out.println(table.get("engineering"));
 
     }
 }
