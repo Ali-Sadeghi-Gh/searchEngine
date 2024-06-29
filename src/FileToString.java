de55454da31be1c0acdcdd87271190fee691b30c
alisad <asgh1381@gmail.com>
2024-06-29 13:32:03 +0330

    add FileToString.java

---
+++ src/FileToString.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileToString {
+
+
+    public static String[] readFile (File file) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String line;
+        StringBuilder sb = new StringBuilder();
+        while ((line = br.readLine()) != null) {
+            sb.append(line.toLowerCase()).append(" ");
+        }
+        return sb.toString().split(" ");
+    }
+
+
+}
