585218ee7bb7213c5487b6e9609c2b84097ec419
omid-hdr <omidheydari2004@gmail.com>
2024-06-30 15:20:55 +0330

    start to complete funct in SearchEngine

--- src/searchEngine/SearchEngine.java
+++ src/searchEngine/SearchEngine.java
@@ -3,15 +3,30 @@ package searchEngine;
 import searchEngine.filters.Filter;
 import searchEngine.tokenizers.Tokenizer;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Vector;
 
 public class SearchEngine<K> {
     private Vector<Filter> filters;
     private Tokenizer tokenizer;
+    private Controller controller;
 
 
-    public SearchEngine(Vector<Filter> filters, Tokenizer tokenizer) {
+    public SearchEngine(Vector<Filter> filters, Tokenizer tokenizer) throws IOException {
         this.filters = filters;
         this.tokenizer = tokenizer;
+        this.controller = Controller.getInstance();
     }
+
+    public void addDoc(HashMap<K, String> data) {
+        // TODO handle input data
+    }
+
+    public String search(Vector<Vector<String>> query) {
+        // Todo search
+
+        return "";
+    }
+
 }
