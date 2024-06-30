914b97175fd35fa4383d9563dafe14a54120151c
omid-hdr <omidheydari2004@gmail.com>
2024-06-30 15:45:36 +0330

    rebase InvertedIndexManager

--- src/searchEngine/InvertedIndexManager.java
+++ src/searchEngine/InvertedIndexManager.java
@@ -1,20 +1,18 @@
 package searchEngine;
 
-import searchEngine.tokenizers.SpaceTokenizer;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Vector;
 
-public class InvertedIndexManager {
-    private final HashMap<String, Vector<String>> invertedIndex;
+public class InvertedIndexManager<K> {
+    private final HashMap<String, Vector<K>> invertedIndex;
 
     public InvertedIndexManager() {
         invertedIndex = new HashMap<>();
     }
 
-    public Vector<String> findDocsByWord(String word) {
+    public Vector<K> findDocsByWord(String word) {
         return invertedIndex.get(word);
     }
 
@@ -24,6 +22,12 @@ public class InvertedIndexManager {
             // addToInvertedIndex(words, child.getName());
         }
         invertedIndex.remove("");
+    }
+
+    public void addData(HashMap<K, String> data) { //TODO keySet
+        for (K k : data.keySet()) {
+
+        }
     }
 
     private void addToInvertedIndex(String[] words, String fileName) {
